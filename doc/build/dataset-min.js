97be68700377781ea5a9949fb85373cf7f7f7d20
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-23 12:29:31 +0800

    sync doc

---
+++ doc/build/dataset-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-08-23 11:37:18 AM*/
+KISSY.add("kg/xlist/3.0.0/dataset",function(){var a=function(a){this.data=a&&a.data||[],this.id=a&&a.id||"_ds_"+Date.now()};return a.prototype.appendData=function(a){this.data=this.data.concat(a)},a.prototype.removeData=function(){this.data=[]},a.prototype.getData=function(){return this.data},a.prototype.setId=function(a){return a?(this.id=a,this.id):void 0},a.prototype.getId=function(){return this.id},a});
