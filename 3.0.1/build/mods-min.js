f8188cd7aaaa0babb5ae642bfe6e1d1bd6e50db0
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-04 19:25:08 +0800

    sync daily version 3.0.1

---
+++ 3.0.1/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-09-04 7:24:44 PM*/
+KISSY.config("modules",{"kg/xlist/3.0.1/index":{requires:["node","event","kg/xscroll/1.1.5/index","kg/xscroll/1.1.5/util"]}});
---
+++ 3.0.1/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/xlist/3.0.1/index': { requires: ['node', 'event', 'kg/xscroll/1.1.5/index', 'kg/xscroll/1.1.5/util']} 
+});
